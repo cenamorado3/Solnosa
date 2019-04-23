aced7c7ec144ab4c3646a98d5c2c7d31532d8794
Christian Enamorado <cenamorado3@gmail.com>
2019-04-23 10:49:58 -0500

    Working out design ideas to see which direction I'd like to take the final overall look of the website.
    Update inclues some CSS for animation transitions and error handling of the contact form's database errors

--- Controllers/ContactFormsController.cs
+++ Controllers/ContactFormsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -25,23 +26,28 @@ namespace MyWebsite.Controllers
             return View();
         }
 
-        // GET: ContactForms/Details/5
-        public ActionResult Details(string id)
+        public ActionResult Error()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ContactForm contactForm = db.ContactForms.Find(id);
-            if (contactForm == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contactForm);
+            return View();
         }
 
+        // GET: ContactForms/Details/5
+        //public ActionResult Details(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ContactForm contactForm = db.ContactForms.Find(id);
+        //    if (contactForm == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(contactForm);
+        //}
+
         // GET: ContactForms/Create
-        public ActionResult Create()
+        public ActionResult Contact()
         {
             return View();
         }
@@ -51,92 +57,105 @@ namespace MyWebsite.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Email,Phone,Reason,Message")] ContactForm contactForm)
+        public ActionResult Contact([Bind(Include = "Email,Phone,Reason,Message")] ContactForm contactForm)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.ContactForms.Add(new ContactForm
+                if (ModelState.IsValid)
                 {
-                    Email = contactForm.Email,
-                    Phone = contactForm.Phone,
-                    Reason = contactForm.Reason,
-                    Seen = "N",
-                    ContactDate = DateTime.Now.ToString(),
-                    Message = contactForm.Message
-                });
-                db.SaveChanges();
-                return RedirectToAction("Submitted");
+                    db.ContactForms.Add(new ContactForm
+                    {
+                        Email = contactForm.Email,
+                        Phone = contactForm.Phone,
+                        Reason = contactForm.Reason,
+                        Seen = "N",
+                        ContactDate = DateTime.Now.ToString(),
+                        Message = contactForm.Message
+                    });
+                    db.SaveChanges();
+                    return RedirectToAction("Submitted");
+                }
+
+                return View(contactForm);
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                System.Threading.Thread.Sleep(1000);
+                return RedirectToAction("Error");
             }
 
 
-
-            return View(contactForm);
         }
 
-        // GET: ContactForms/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ContactForm contactForm = db.ContactForms.Find(id);
-            if (contactForm == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contactForm);
-        }
 
-        // POST: ContactForms/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Email,Phone,Reason,Seen,ContactDate,Message")] ContactForm contactForm)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(contactForm).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(contactForm);
-        }
 
-        // GET: ContactForms/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ContactForm contactForm = db.ContactForms.Find(id);
-            if (contactForm == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contactForm);
-        }
+        //    // GET: ContactForms/Edit/5
+        //    public ActionResult Edit(string id)
+        //    {
+        //        if (id == null)
+        //        {
+        //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //        }
+        //        ContactForm contactForm = db.ContactForms.Find(id);
+        //        if (contactForm == null)
+        //        {
+        //            return HttpNotFound();
+        //        }
+        //        return View(contactForm);
+        //    }
 
-        // POST: ContactForms/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            ContactForm contactForm = db.ContactForms.Find(id);
-            db.ContactForms.Remove(contactForm);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //    // POST: ContactForms/Edit/5
+        //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Edit([Bind(Include = "Email,Phone,Reason,Seen,ContactDate,Message")] ContactForm contactForm)
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            db.Entry(contactForm).State = EntityState.Modified;
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
+        //        return View(contactForm);
+        //    }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //    // GET: ContactForms/Delete/5
+        //    public ActionResult Delete(string id)
+        //    {
+        //        if (id == null)
+        //        {
+        //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //        }
+        //        ContactForm contactForm = db.ContactForms.Find(id);
+        //        if (contactForm == null)
+        //        {
+        //            return HttpNotFound();
+        //        }
+        //        return View(contactForm);
+        //    }
+
+        //    // POST: ContactForms/Delete/5
+        //    [HttpPost, ActionName("Delete")]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult DeleteConfirmed(string id)
+        //    {
+        //        ContactForm contactForm = db.ContactForms.Find(id);
+        //        db.ContactForms.Remove(contactForm);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    protected override void Dispose(bool disposing)
+        //    {
+        //        if (disposing)
+        //        {
+        //            db.Dispose();
+        //        }
+        //        base.Dispose(disposing);
+        //    }
+        //}
     }
 }
